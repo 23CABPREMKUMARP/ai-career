cbcb89e2cff4368ea21310e91515c928837dea4e
PREM KUMAR PERUMAL <139529932+23CABPREMKUMARP@users.noreply.github.com>
2025-11-02 10:10:36 +0530

    Your descriptive commit message

---
+++ proxy.js
@@ -0,0 +1,28 @@
+// proxy.ts
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/resume(.*)",
+  "/interview(.*)",
+  "/ai-cover-letter(.*)",
+  "/onboarding(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|.*\\..*).*)", // exclude _next and static assets
+    "/(api|trpc)(.*)"
+  ]
+};
